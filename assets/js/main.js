a75c71ece22f8e393bf71dc0fa5112063c4211e2
Godwin Kachi <godwinkachi@hotmail.com>
2023-07-15 12:00:46 +0100

    consolidated the app

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+const screen = document.querySelector('.calculator__display')
+const nhs = document.querySelectorAll('.btn-keys')
+console.log(screen.innerHTML)
+
+// console.log(nhs)
+
+// for (i = 0; elelments = nhs[i]; i++) {
+//     i.addEventListener('click', () => {
+//         console.log(i.innerHTML)
+//     })
+// }
+
+
+// nhs.addEventListener('click', () => {
+//     for (i = 0; elements = nhs[i]; i++){
+//         console.log(elements.innerHTML)
+//     }
+// })
+
+// nhs.addEventListener('click', () => {
+//     console.log('this')
+// })
+
+// nhs.forEach(item => {
+//     item.addEventListener('click', () => {
+//         console.log(item.innerHTML)
+
+//     })
+// })
+
+
+function show(n) {
+    console.log(n)
+}
