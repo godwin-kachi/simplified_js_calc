1d6759860f6c10ffd23428bd764cad14bbe3172a
Godwin Kachi <godwinkachi@hotmail.com>
2023-07-17 02:23:54 +0100

    70% functionality complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,44 @@
+
 const screen = document.querySelector('.calculator__display')
 const nhs = document.querySelectorAll('.btn-keys')
-console.log(screen.innerHTML)
+const dot = document.querySelector('.dots')
+const operators = document.querySelectorAll('.btn-operator')
+const enter = document.querySelector('.equal')
+const bksp = document.querySelector('.right')
+const clear = document.querySelector('.rigt')
+
+// console.log(operators)
+
+
+
+dot.addEventListener('click', () => {
+    if (screen.innerHTML.includes('.')) {
+        dot.classList.add('is-depressed')
+        // console.log('Decimal point already exists')
+    }else{
+        screen.innerHTML += '.'
+    }
+})
+
+bksp.addEventListener('click', () => {
+    if(screen.innerHTML.length > 1){
+        screen.innerHTML = screen.innerHTML.substring(0, screen.innerHTML.length - 1)
+    }
+})
+
+clear.addEventListener('click', () => {
+    screen.innerHTML = '0'
+})
+
+operators.forEach(items => {
+    items.addEventListener('click', () => {
+        if(screen.innerHTML.includes(items.innerText)){
+            items.classList.add('is-depressed')
+        }else{
+            screen.innerHTML += items.innerText
+        }
+    })
+})
 
 // console.log(nhs)
 
@@ -21,14 +59,20 @@ console.log(screen.innerHTML)
 //     console.log('this')
 // })
 
-// nhs.forEach(item => {
-//     item.addEventListener('click', () => {
-//         console.log(item.innerHTML)
 
-//     })
-// })
+nhs.forEach(item => {
+    item.addEventListener('click', () => {
+        // console.log(item.innerHTML)
+        if(screen.innerHTML == 0) {
+            screen.innerHTML = item.innerHTML
+        } else {
+            screen.innerHTML += item.innerHTML
+        }
+
+    })
+})
 
 
-function show(n) {
-    console.log(n)
-}
+// function show(n) {
+//     console.log(n)
+// }
