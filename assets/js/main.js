2c9e83f062c9199c907f5fbdef8b5d2a93e7a417
Godwin Kachi <godwinkachi@hotmail.com>
2023-07-19 00:42:18 +0100

    Applied prettier formatter to my codes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,61 +1,57 @@
+const screen = document.querySelector(".calculator__display");
+const nhs = document.querySelectorAll(".btn-keys");
+const dot = document.querySelector(".dots");
+const operators = document.querySelectorAll(".btn-operator");
+const enter = document.querySelector(".equal");
+const bksp = document.querySelector(".right");
+const clear = document.querySelector(".rigt");
 
-const screen = document.querySelector('.calculator__display')
-const nhs = document.querySelectorAll('.btn-keys')
-const dot = document.querySelector('.dots')
-const operators = document.querySelectorAll('.btn-operator')
-const enter = document.querySelector('.equal')
-const bksp = document.querySelector('.right')
-const clear = document.querySelector('.rigt')
+enter.addEventListener("click", () => {
+  screen.innerHTML = eval(screen.innerHTML);
+});
 
+dot.addEventListener("click", () => {
+  if (screen.innerHTML.includes(".")) {
+    dot.classList.add("is-depressed");
+  } else {
+    screen.innerHTML += ".";
+  }
+});
 
-enter.addEventListener('click', () => {
-    screen.innerHTML = eval(screen.innerHTML)
-})
+enter.addEventListener("click", () => {
+  let p = eval(screen.innerHTML);
+  screen.innerHTML = p;
+});
 
+bksp.addEventListener("click", () => {
+  if (screen.innerHTML.length > 1) {
+    screen.innerHTML = screen.innerHTML.substring(
+      0,
+      screen.innerHTML.length - 1
+    );
+  }
+});
 
-dot.addEventListener('click', () => {
-    if (screen.innerHTML.includes('.')) {
-        dot.classList.add('is-depressed')
-    }else{
-        screen.innerHTML += '.'
+clear.addEventListener("click", () => {
+  screen.innerHTML = "0";
+});
+
+operators.forEach((items) => {
+  items.addEventListener("click", () => {
+    if (screen.innerHTML.includes(items.innerText)) {
+      items.classList.add("is-depressed");
+    } else {
+      screen.innerHTML += items.innerText;
     }
-})
+  });
+});
 
-enter.addEventListener('click', ()=> {
-    let p = eval(screen.innerHTML)
-    screen.innerHTML = p
-})
-
-bksp.addEventListener('click', () => {
-    if(screen.innerHTML.length > 1){
-        screen.innerHTML = screen.innerHTML.substring(0, screen.innerHTML.length - 1)
+nhs.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (screen.innerHTML == 0) {
+      screen.innerHTML = item.innerHTML;
+    } else {
+      screen.innerHTML += item.innerHTML;
     }
-})
-
-clear.addEventListener('click', () => {
-    screen.innerHTML = '0'
-})
-
-operators.forEach(items => {
-    items.addEventListener('click', () => {
-        if(screen.innerHTML.includes(items.innerText)){
-            items.classList.add('is-depressed')
-        }else{
-            screen.innerHTML += items.innerText
-        }
-    })
-})
-
-
-
-nhs.forEach(item => {
-    item.addEventListener('click', () => {
-        if(screen.innerHTML == 0) {
-            screen.innerHTML = item.innerHTML
-        } else {
-            screen.innerHTML += item.innerHTML
-        }
-
-    })
-})
-
+  });
+});
